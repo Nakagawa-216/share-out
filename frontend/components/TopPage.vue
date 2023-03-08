85ccbe258868162e24b58b091b98c1ef6375a210
Nakagawa-216 <rarai0626@gmail.com>
2023-03-08 20:35:08 +0900

    complete toppage

---
+++ frontend/components/TopPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div>
+      <router-link to="/register">新規登録</router-link>
+      <router-link to="/login">ログイン</router-link>
+      <router-link to="/about">About</router-link>
+    </div>
+    <div>
+      <h1>ShareOut</h1>
+      <p>Share your workout,</p>
+      <p>and make better</p>
+    </div>
+  </div>
+</template>
