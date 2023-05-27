e5a7b51f31d7fe64e0ae1d507505c3dba000c44f
Nakagawa-216 <rarai0626@gmail.com>
2023-05-27 13:20:18 +0900

    Added user's email and name to MyPage

--- frontend/pages/user/MyPage.vue
+++ frontend/pages/user/MyPage.vue
@@ -1,11 +1,22 @@
 <template>
   <div>
     <h1>マイページ</h1>
+    <p>Email: {{ user.email }}</p>
+    <p>Name: {{ user.name }}</p>
   </div>
 </template>
 
 <script>
   export default{
-    name: 'MyPage'
+    name: 'MyPage',
+    data(){
+      return {
+        user: {}
+      };
+    },
+    async mounted() {
+      const response = await this.$axios.get('/api/v1/users');
+      this.user = response.data;
+    }
   }
 </script>
