b0969c0e8c2ded77ecad36fe28c605e455c0d91e
Nakagawa-216 <rarai0626@gmail.com>
2023-06-14 18:30:10 +0900

    Add index view to App.vue

--- frontend/pages/App.vue
+++ frontend/pages/App.vue
@@ -1,13 +1,25 @@
 <template>
   <div id="app">
     <h1>POSTS</h1>
+    <ul v-for="post in posts" :key="post.id">
+      <li style="text-align: left;">{{ post.title }}</li>
+      <li style="text-align: left;">{{ post.body }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
-
 export default {
   name: "App",
+  data() {
+    return {
+      posts: []
+    }
+  },
+  async mounted(){
+    const response = await this.$axios.$get('http://localhost:8000/api/v1/posts')
+    this.posts = response
+  },
   methods: {
   }
 };
