d8660b31a5e40ba993fedfd625bbc34fbee24dca
Nakagawa-216 <rarai0626@gmail.com>
2023-06-25 09:39:34 +0900

    Add EditPost.vue

---
+++ frontend/pages/post/EditPost.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Edit Post</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: '',
+      body: ''
+    }
+  },
+  async mounted() {
+    const url = 'http://localhost:8000/api/v1/posts' +  
+    const postData = await this.$axios.$get('http://localhost:8000/api/v1/posts/' + id)
+  }
+}
+</script>
--- frontend/pages/post/_id.vue
+++ frontend/pages/post/_id.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
     <div>
+      <div v-if="ownPost">
+        <button @click="$router.push('/post/EditPost')">EditPost</button>
+      </div>
+    </div>
+    <div>
       <h2>Title</h2>
       {{ post.title }}
     </div>
@@ -15,13 +20,18 @@
 export default {
   data() {
     return {
-      post: []
+      post: [],
+      ownPost: false
     }
   },
   async mounted(){
     const id = this.$route.params.id
     const response = await this.$axios.$get('http://localhost:8000/api/v1/posts/' + id)
     this.post = response
+
+    if (this.post.user_id === this.$auth.user.data.id) {
+      this.ownPost = true;
+    }
   }
 }
 </script>
